8cdd408b4645d611ab7efe3fe543fee085e1525d
Ashnita Bali <ashnita01@gmail.com>
2019-01-07 21:34:38 +0000

    Theme service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'theming-with-custom-css-properties';
+
+  // Create an instance of the form control as a class property
+  // Set its initial value
+  // We get immediate access to listen for
+  // update and validate the state of form input
+  darkTheme: AbstractControl = new FormControl(false);
+
+  constructor(private themeService: ThemeService) {
+    this.darkTheme.valueChanges.subscribe(value => {
+      if(value) {
+        this.themeService.toggleDark();
+      } else {
+        this.themeService.toggleLight();
+      }
+    })
+  }
+
+  ngOnInit() {
+    console.log(this.darkTheme);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
